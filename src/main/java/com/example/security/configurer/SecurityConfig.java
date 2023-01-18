0ccccac7cf3de4783f02f2dbe927be1b164c918b
Step7439 <zamashikovs@mail.ru>
2023-01-18 16:35:29 +0900

    Add OAuth2

--- src/main/java/com/example/security/configurer/SecurityConfig.java
+++ src/main/java/com/example/security/configurer/SecurityConfig.java
@@ -1,33 +1,48 @@
 package com.example.security.configurer;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableMethodSecurity(securedEnabled = true, prePostEnabled = true, jsr250Enabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+    @Bean
+    public PasswordEncoder encoder(){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
-                .withUser("Step").password("{noop}password").authorities("read", "write")
+                .withUser("Step")
+                .password(encoder().encode("password"))
+                .roles("READ", "WRITE")
                 .and()
-                .withUser("Dima").password("{noop}password").authorities("read")
+                .withUser("Dima")
+                .password(encoder().encode("password2"))
+                .authorities("read","write")
                 .and()
-                .withUser("Vasy").password("{noop}password").authorities("write");
+                .withUser("Vasy")
+                .password(encoder().encode("password3"))
+                .authorities("write");
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.formLogin()
-                .and()
-                .authorizeRequests().antMatchers("/hello").permitAll()
-                .and()
-                .authorizeRequests().antMatchers("/login").hasAuthority("read")
-                .and()
-                .authorizeRequests().antMatchers("/read").hasAuthority("read")
-                .and()
-                .authorizeRequests().anyRequest().authenticated();
-
-    }
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.formLogin()
+//                .and()
+//                .authorizeRequests().antMatchers("/hello").permitAll()
+//                .and()
+//                .authorizeRequests().antMatchers("/login").hasAuthority("read")
+//                .and()
+//                .authorizeRequests().antMatchers("/read").hasAuthority("read")
+//                .and()
+//                .authorizeRequests().anyRequest().authenticated();
+//
+//    }
 }
--- src/main/java/com/example/security/controler/SecurityControler.java
+++ src/main/java/com/example/security/controler/SecurityControler.java
@@ -1,7 +1,12 @@
 package com.example.security.controler;
 
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.security.RolesAllowed;
 
 @RestController
 public class SecurityControler {
@@ -10,16 +15,27 @@ public class SecurityControler {
     public String hello(){
         return "Heloo";
     }
-    @GetMapping("/login")
+    @PreAuthorize("hasAnyRole('WRITE')")
+    @GetMapping("/avto")
     public String login(){
         return "Hello user !!!";
     }
+    @RolesAllowed({"ROLE_READ"})
     @GetMapping("/read")
     public String read(){
         return "read";
     }
+    @Secured({"ROLE_WRITE"})
     @GetMapping("/write")
     public String write(){
         return "write";
+    }
+    @GetMapping("/hi")
+    public String hi(){
+        return "hi";
+    }
+    @PostMapping("/hi")
+    public String hiPost(){
+        return "hiPost";
     }
 }
